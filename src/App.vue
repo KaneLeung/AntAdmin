68ea5c28102fdecfcc5c07c3891c971f7c8da834
KaneLeung <kk.xiaolan@gmail.com>
2020-12-02 15:36:29 +0800

    :alien:添加ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <a-layout id="layout-content">
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"> Content </a-layout-content>
+      <a-layout-footer style="background: #fff;">Footer</a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { UserOutlined, VideoCameraOutlined, UploadOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  },
+  data() {
+    return {
+      selectedKeys: ['1'],
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+#layout-content {
+  height: 100vh;
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+    &:hover {
+      color: #1890ff;
+    }
+  }
+  .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
   }
 }
-</script>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import router from '/@/router'
 import App from '/@/App.vue'
 import '/@/index.css'
 import "ant-design-vue/dist/antd.css";
-import { Button } from "ant-design-vue";
+import {
+  Button,
+  Layout,
+  Menu
+} from "ant-design-vue";
 
 const app = createApp(App)
 app.use(Button)
+app.use(Layout).use(Menu)
 app.use(router).mount('#app')
